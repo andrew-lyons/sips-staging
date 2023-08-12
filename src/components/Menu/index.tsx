3c440ec963f4d4dbf9f2eebaa942117f19d90c52
Andrew Lyons <aslyons001@gmail.com>
2023-08-12 18:58:22 -0400

    updated menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import './index.scss';
 
 export interface MenuCategory {
@@ -9,6 +9,7 @@ export interface MenuCategory {
 const Menu: React.FC = () => {
   const [menuResult, setMenuResult] = useState<MenuCategory[]>([]);
   const [smallMenuGroups, setSmallMenuGroups] = useState<MenuCategory[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     fetch('https://sips-email-server.vercel.app/api/catalog')
@@ -20,36 +21,50 @@ const Menu: React.FC = () => {
       const smallerResults = result.filter(f => f.items.length <= 2);
       setMenuResult(biggerResults);
       setSmallMenuGroups(smallerResults);
+      setLoading(false);
+    })
+    .catch(e => {
+      console.log('error!', e);
+      setLoading(false);
     });
   }, [])
 
   return (
     <section className='menu'>
-      <h2 className='menu-title'>Menu</h2>
+      {
+        loading
+        ? <div id="loading" />
+        : (
+          <Fragment>
+            <h2 className='menu-title'>Menu</h2>
 
-      <div className='menu-section-wrapper'>
-        { menuResult.map((r: { name: string, items: string[] }, idx) => {
-          return (
-            <div key={`${r.name}-menusection-${idx}`} className='menu-section'>
-              <h3>{ r.name }</h3>
-
-              { r.items.map((i, itemIdx)=> {
+            <div className='menu-section-wrapper'>
+              { menuResult.map((r: { name: string, items: string[] }, idx) => {
                 return (
-                  <p key={`${i}-item-${itemIdx}`}>{ i }</p>
+                  <div key={`${r.name}-menusection-${idx}`} className='menu-section'>
+                    <h3>{ r.name }</h3>
+
+                    { r.items.map((i, itemIdx)=> {
+                      return (
+                        <p key={`${i}-item-${itemIdx}`}>{ i }</p>
+                      )
+                    })}
+                  </div>
                 )
               })}
             </div>
-          )
-        })}
-      </div>
 
-      <h2 className='menu-title'>Also...</h2>
+            <h2 className='menu-title'>Also...</h2>
 
-      <div className='menu-section-wrapper menu-section-wrapper-smallerwrapper'>
-        { smallMenuGroups.map((r: { name: string, items: string[] }, idx) => 
-          <h3 className='menu-section-wrapper-smaller' key={`${r.name}-alsosection-${idx}`}>{ r.name }</h3>
-        )}
-      </div>
+            <div className='menu-section-wrapper menu-section-wrapper-smallerwrapper'>
+              { smallMenuGroups.map((r: { name: string, items: string[] }, idx) => 
+                <h3 className='menu-section-wrapper-smaller' key={`${r.name}-alsosection-${idx}`}>{ r.name }</h3>
+              )}
+            </div>
+          </Fragment>
+        )
+      }
+
     </section>
   )
 }
